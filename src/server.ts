aaaaca001ec43136efbdbce39b2325496081fbf1
Theodora Nascimento <theodoram@ymail.com>
2024-10-17 22:24:04 -0400

    add error message when creating a movie

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -17,6 +19,29 @@ app.get("/movies", async (_, res) => {
     });
     res.json(movies);
 });
+
+app.post("/movies", async(req, res) => {
+
+   // console.log(`Conteudo do body enviado na requisicao: ${req.body.title}`)
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try{
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date) //nesse caso pode paasar um string aqui
+            }
+        });
+    }catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+    }
+    res.status(201).send();
+
+})
 
 app.listen(port, () => {
   console.log(`Servidor em execução na porta http://localhost:${port}`);
