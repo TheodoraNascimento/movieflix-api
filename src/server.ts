6919f963fb3622b2cc1f10f6d14c38f4cb4940d1
Theodora Nascimento <theodoram@ymail.com>
2024-10-17 23:28:18 -0400

    added movies update

--- src/server.ts
+++ src/server.ts
@@ -51,8 +51,40 @@ app.post("/movies", async(req, res) => {
         return res.status(500).send({ message: "Falha ao cadastrar um filme" });
     }
     res.status(201).send();
+});
 
+app.put("/movies/:id", async (req, res) => {
+    //console.log(req.params.id);
+    const id = Number(req.params.id);
+
+    try{
+    const movie = await prisma.movie.findUnique({
+        where: {
+            id
+        }
+    });
+
+    if(!movie){
+        return res.status(404).send({ message: "Movie not found" });
+    }
+    const data = { ...req.body };
+    //console.log(data);
+    data.release_date = data.release_date ? new Date(data.release_date): undefined;
+    
+    await prisma.movie.update({
+        where: {
+            id
+        },
+        data: data
+    });
+}catch(error){
+    return res.status(500).send( {message: "Error when trying to update movie info"}) //500 erro no servidor
+}
+
+    res.status(200).send();
+    
 })
+
 
 app.listen(port, () => {
   console.log(`Servidor em execução na porta http://localhost:${port}`);
